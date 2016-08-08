902a567725d5c0b3203906e19659ac37bfdeea67
Marcus Rossel <marcusrossel@icloud.com>
2016-08-08 22:13:53 +0200

    Initial Commit

---
+++ Kaleidoscope/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Kaleidoscope
+//
+//  Created by Marcus Rossel on 08.08.16.
+//  Copyright © 2016 Marcus Rossel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
