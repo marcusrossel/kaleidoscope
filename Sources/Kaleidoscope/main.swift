e793bc614819ec19fdf01a49056578613a14201f
Marcus Rossel <marcusrossel@icloud.com>
2019-09-12 22:22:59 +0200

    Implement example of an LLVM-built function.

--- Sources/Kaleidoscope/main.swift
+++ Sources/Kaleidoscope/main.swift
@@ -24,3 +24,93 @@ let parser = Parser(tokens: lexer)
 let ast = try parser.parseFile()
 
 print(ast)
+
+
+
+import CLLVM
+
+// Helper for converting proper booleans back into numbers.
+extension BinaryInteger {
+    
+    init(_ bool: Bool) {
+        self.init()
+        self = bool ? 1 : 0
+    }
+}
+
+let module: LLVMModuleRef = LLVMModuleCreateWithName("main")
+
+var parameters: [LLVMTypeRef?] = [LLVMInt32Type(), LLVMInt32Type()]
+
+let prototype: LLVMTypeRef = LLVMFunctionType(
+    LLVMInt32Type(),
+    &parameters,
+    UInt32(parameters.count),
+    LLVMBool(false)
+)
+
+let function: LLVMValueRef = LLVMAddFunction(module, "sum", prototype)
+
+let entry: LLVMBasicBlockRef = LLVMAppendBasicBlock(function, "entry")
+
+let builder: LLVMBuilderRef = LLVMCreateBuilder()
+
+LLVMPositionBuilderAtEnd(builder, entry)
+
+let result: LLVMValueRef = LLVMBuildAdd(
+    builder,
+    LLVMGetParam(function, 0),
+    LLVMGetParam(function, 1),
+    "result"
+)
+
+LLVMBuildRet(builder, result)
+
+var error: UnsafeMutablePointer<Int8>?
+
+LLVMVerifyModule(
+    module,
+    LLVMAbortProcessAction,
+    &error
+)
+
+if let error = error { LLVMDisposeMessage(error) }
+
+error = nil
+
+if (LLVMWriteBitcodeToFile(module, "/Users/marcus/Desktop/a.bc") != 0) {
+    fatalError("Error emitting bitcode.")
+}
+
+/*
+var engine: LLVMExecutionEngineRef?
+
+LLVMLinkInMCJIT()
+LLVMInitializeNativeTarget()
+
+if (LLVMCreateExecutionEngineForModule(&engine, module, &error) != 0) {
+    fatalError("Failed to create execution engine.")
+}
+
+if let error = error {
+    print("Error: \(error)")
+    LLVMDisposeMessage(error)
+    fatalError()
+}
+
+var arguments: [LLVMGenericValueRef?] = [
+    LLVMCreateGenericValueOfInt(LLVMInt32Type(), 12, LLVMBool(false)),
+    LLVMCreateGenericValueOfInt(LLVMInt32Type(), 42, LLVMBool(false))
+]
+
+let output: LLVMGenericValueRef = LLVMRunFunction(
+    engine,
+    function,
+    UInt32(arguments.count),
+    &arguments
+)
+
+let usableOutput: UInt64 = LLVMGenericValueToInt(output, LLVMBool(false))
+
+print("\(usableOutput)")
+*/
